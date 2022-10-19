3cb7f69f3a623ea3129f25e0759c7c39df3f85f8
William3349 <william12@mesoamericana.edu.gt>
2022-10-19 11:00:04 -0600

    creacion de login

---
+++ lib/login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class MyAppForm extends StatefulWidget {
+  const MyAppForm({super.key});
+
+  @override
+  State<MyAppForm> createState() => _MyAppFormState();
+}
+
+class _MyAppFormState extends State<MyAppForm> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 90.0
+        ),
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(                
+                'SOLARES energía sustentable',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontFamily: 'NerkoOne',
+                  fontSize: 30.0
+                ),
+              ),
+              Divider(
+                height: 25.0,
+              ),
+              CircleAvatar(
+                radius: 100.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage('images/logo.png'),
+              ),
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontFamily: 'NerkoOne',
+                  fontSize: 50.0
+                ),
+              ),
+              
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                  hintText:'Ingrese su suario',
+                  labelText: 'Ingrese su usuario',
+                  suffixIcon: Icon(
+                    Icons.verified_user
+                  ),
+                  border:  OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                  )
+                ),                
+              ),
+              Divider(
+                height: 25.0,
+              ),
+
+              TextField(
+                enableInteractiveSelection: false,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  labelText: 'Contraseña',
+                  suffixIcon: Icon(Icons.lock_outline),
+                  border: OutlineInputBorder(
+                   borderRadius: BorderRadius.circular(20.0)
+                  )
+                ),
+              ),
+              Divider(
+                height: 25.0,
+              ),
+           MaterialButton(
+    minWidth: 200.0,
+    height: 50.0,
+    onPressed: () {},
+    color: Color.fromARGB(255, 0, 170, 228),
+    child: Text('Iniciar secion', style: TextStyle(color: Color.fromARGB(255, 20, 19, 19))),
+   ),
+            ],
+          )
+        ],        
+      ),
+    );
+  }
+}
